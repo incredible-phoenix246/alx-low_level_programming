dbad893361f03b93667bbdcd45eeac156338c193
Ayomikun <ayomikuntemitope.araoye@gmail.com>
2023-09-07 10:35:50 -0700

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*learning hoe to use the put function*/
+/**
+*main - entry point
+*
+*returns 0
+*/
+int	 main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

4d1d234821dd503b3d7662897b9fbc001e303c63
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-12 22:47:37 +0100

    doing hard things

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Thid is the entry point of the program
+ * This program, prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (indicating successful)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}

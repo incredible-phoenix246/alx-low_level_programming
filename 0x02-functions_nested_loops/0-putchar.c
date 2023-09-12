df671f03751f61e39614bf3035d39e0e5147fccc
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-12 22:55:02 +0100

    doing hard things

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Thid is the entry point of the program
- * This program, prints _putchar, followed by a new line.
+ * main- Entry point
  *
- * Return: Always 0 (indicating successful)
+ * Return: Always 0 (Succes)
  */
-
 int main(void)
 {
 	putchar('_');

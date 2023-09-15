63dfd416d8fc8ce0a048a20fe511e0bbe4a43f0c
Ayomikun <ayomikuntemitope.araoye@gmail.com>
2023-09-14 20:43:31 -0700

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,20 +7,20 @@
  */
 void more_numbers(void)
 {
-    int a, b;
+	int a, b;
 
-    for (a = 0; a < 10; a++)
-    {
-        for (b = 0; b <= 14; b++)
-        {
-            if (b > 9)
-            {
-                _putchar((b / 10) + '0');
-            }
+	for (a = 0; a < 10; a++
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			{
+				_putchar((b / 10) + '0');
+			}
 
-            _putchar((b % 10) + '0');
-        }
+			_putchar((b % 10) + '0');
+		}
 
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }

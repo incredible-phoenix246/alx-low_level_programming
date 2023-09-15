00111df97872602adf89d9c35c964835414e3248
Ayomikun <ayomikuntemitope.araoye@gmail.com>
2023-09-14 20:55:40 -0700

    na

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,23 @@
  */
 void print_diagonal(int n)
 {
-    int a, b;
+	int a, b;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (a = 0; a < n; a++)
-        {
-            for (b = 0; b < a; b++)
-            {
-                _putchar(32);
-            }
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(32);
+			}
 
-            _putchar(92);
-            _putchar('\n');
-        }
-    }
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
 }

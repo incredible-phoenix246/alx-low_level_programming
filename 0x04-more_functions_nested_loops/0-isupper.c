deb510aea59eb15d6677a4c585d75e8be5acebdd
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-15 02:21:22 +0100

    started

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - Check for uppercase
- * @c: Range
+ * _isupper - uppercase letters
+ * @c: character to check
  *
- * Return: Could be 1 or 0 (not sure)
+ * Return: 0 or 1
  */
 int _isupper(int c)
-{
-    if (c >= 65 && c <= 90)
-    {
-        return (1);
-    }
 
-    return (0);
+{
+    if (c >= 'A' && c <= 'Z')
+        return (1);
+    else
+        return (0);
 }

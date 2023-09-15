00157c7010c008d51a53b206f1eda7580bbcdcf8
Ayomikun <ayomikuntemitope.araoye@gmail.com>
2023-09-14 21:12:05 -0700

    nr

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,27 +8,27 @@
  */
 void print_triangle(int size)
 {
-    int a, b, c;
+	int a, b, c;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (a = 0; a < size; a++)
-        {
-            for (b = size - a; b > 1; b--)
-            {
-                _putchar(32);
-            }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = size - a; b > 1; b--)
+			{
+				_putchar(32);
+			}
 
-            for (c = 0; c <= a; c++)
-            {
-                _putchar(35);
-            }
+			for (c = 0; c <= a; c++)
+			{
+				_putchar(35);
+			}
 
-            _putchar('\n');
-        }
-    }
+			_putchar('\n');
+		}
+	}
 }

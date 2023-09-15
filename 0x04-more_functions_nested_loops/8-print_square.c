4faf9da2b5913618143d33ca281268eb8e444c4b
Ayomikun <ayomikuntemitope.araoye@gmail.com>
2023-09-14 21:08:11 -0700

    nj

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,22 +8,22 @@
  */
 void print_square(int size)
 {
-    int a, b;
+	int a, b;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (a = 0; a < size; a++)
-        {
-            for (b = 0; b < size; b++)
-            {
-                _putchar(35);
-            }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
 
-            _putchar('\n');
-        }
-    }
+			_putchar('\n');
+		}
+	}
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,34 +8,34 @@
  */
 int main(void)
 {
-    int a;
+	int a;
 
-    for (a = 1; a <= 100; a++)
-    {
-        if ((a % 3 == 0) && (a % 5 == 0))
-        {
-            printf("FizzBuzz");
-        }
-        else if (a % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (a % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", a);
-        }
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
 
-        if (a != 100)
-        {
-            printf(" ");
-        }
-    }
+		if (a != 100)
+		{
+			printf(" ");
+		}
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
